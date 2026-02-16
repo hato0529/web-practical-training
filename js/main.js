12f9bb1efe356ba9768ac0f09bcda0b14120df6d
Haruto <M2520002@ngo.ohara.ac.jp>
2026-02-16 12:28:16 +0900

    products2の仮完成

--- js/main.js
+++ js/main.js
@@ -59,10 +59,10 @@ document.addEventListener('DOMContentLoaded', () => {
     glow.style.top = `${y}px`;
     
     // 3D傾斜効果
-    const centerX = rect.width / 2;
-    const centerY = rect.height / 2;
-    const rotateX = (y - centerY) / 10;
-    const rotateY = (centerX - x) / 10;
+    // const centerX = rect.width / 2;
+    // const centerY = rect.height / 2;
+    // const rotateX = (y - centerY) / 10;
+    // const rotateY = (centerX - x) / 10;
     
     card.style.transform = `
       perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale3d(1.02, 1.02, 1.02)`;
@@ -73,3 +73,10 @@ document.addEventListener('DOMContentLoaded', () => {
     card.style.transform = 'perspective(1000px) rotateX(0) rotateY(0) scale3d(1, 1, 1)';
   });
 });
+
+
+
+
+
+
+
